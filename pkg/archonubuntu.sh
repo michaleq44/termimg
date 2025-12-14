45fcd14401353295eaf15ed0448bb7ae01f3eb76
michaleq44 <michalokul@gmail.com>
2025-12-14 15:51:39 +0100

    permissions?

--- pkg/archonubuntu.sh
+++ pkg/archonubuntu.sh
@@ -2,6 +2,7 @@
 pacman -Syu --noconfirm
 pacman -S git ncurses sudo --noconfirm
 cd /termimg/pkg || exit 1
-useradd -M michaleq || true
+useradd -M michaleq || exit 1
+chown -R michaleq:michaleq /termimg
 runuser -l michaleq -c 'makepkg -d'
 rm -rf pkg src PKGBUILD archonubuntu.sh
